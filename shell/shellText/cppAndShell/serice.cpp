fd9420a8444d84724112f14dd839e71e41cf7742
Muiiii <lnz_ran@qq.com>
2023-08-31 10:21:07 +0800

    [add] cpp and shel file

---
+++ shell/shellText/cppAndShell/serice.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+	printf("hello, world!\n");
+	return 0;
+}
